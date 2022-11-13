013f166051d336776c4348c0699ac41b541c55cd
Andrew Rather <andrew.rather5@gmail.com>
2022-11-13 00:28:40 -0500

    fix prototype

--- python/main.py
+++ python/main.py
@@ -38,6 +38,8 @@ def passes(state, knowledge: list[Knowledge], hand_size:int):
                     return True
         return False
     def filter_attribute(cards, attribute):
+        if not attribute:
+            return cards
         return [i for i in cards if attribute in i]
     def question_asked_fail(e:Knowledge, player):
         color = list(ColorPossibilities.intersection(e.question_asked))
@@ -46,18 +48,37 @@ def passes(state, knowledge: list[Knowledge], hand_size:int):
         color = color[0] if color else None
         number = number[0] if number else None
         stone = stone[0] if stone else None
-        final = filter_attribute(player, color)
-        final = filter_attribute(final, number)
-        final = filter_attribute(final, stone)
-        if len(final) != element.num:
-            return True
+        if element.num > 0:
+            final = filter_attribute(player, color)
+            final = filter_attribute(final, number)
+            final = filter_attribute(final, stone)
+            if len(final) != element.num:
+                return True
         return False
+    def has_invalid_non_cards(e:Knowledge, player):
+        if e.num == 0:
+            color = list(ColorPossibilities.intersection(e.question_asked))
+            number = list(NumberPossibilities.intersection(e.question_asked))
+            stone = list(StonePossibilities.intersection(e.question_asked))
+            color = color[0] if color else None
+            number = number[0] if number else None
+            stone = stone[0] if stone else None
+            if color and len([i for i in player if color in i]) > 0:
+                return True
+            if number and len([i for i in player if number in i]) > 0:
+                return True
+            if stone and len([i for i in player if stone in i]) > 0:
+                return True
+            return False
+
     facedown = state[0]
     players = state[1:]
     players_split = list(it.zip_longest(*[iter(players)] * hand_size))
     for element in knowledge:
         player = set(players_split[element.player_num-1])
         if explicit_card_missing(element, player):
+            return False
+        if has_invalid_non_cards(element, player):
             return False
         if question_asked_fail(element, player):
             return False
@@ -74,17 +95,23 @@ def main():
     face_up = set((str_to_card_tup(_str) for _str in ["B2P", "Y1P", "G2P"]))
     deck = set(deck) - set(player_cards) - set(face_up)
     knowledge: list[Knowledge] = []
-    states = [i for i in it.permutations(deck, len(deck))]
-    print(len(states))
-    while len(states) > 1:
-        new_know = input()
+    states = it.permutations(deck, len(deck))
+    while True:
+        print("Send knowledge in format <Player Number>:<Number of cards>,<Question Asked>[<Comma Seperated Explicit Cards>]")
+        new_know = input("  >")
         know = parse_new_knowledge(new_know)
         knowledge.append(know)
-        states = [state for state in states if passes(state, knowledge, 8)]
-        print(len(states))
-        if len(states) == 0:
+        states = (state for state in states if passes(state, knowledge, 8))
+        state1 = next(states, None)
+        state2 = next(states, None)
+        if state1 is None:
             print("Contradiction")
             break
+        if state2 is None:
+            print("Solved!")
+            print(state1[0])
+            break
+        
 
 if __name__ == "__main__":
     main()
